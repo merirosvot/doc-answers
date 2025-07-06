c84c3bb90efc82ca4dd3c2cd1e9d9c6c02d5b04c
merirosvot <merirosvot@outlook.com>
2025-07-07 00:01:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 from openai import OpenAI
 
 # Show title and description.
@@ -11,7 +12,7 @@ st.title("📄 Document question answering")
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.secrets["OPENAI_API_KEY"] 
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 #openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
