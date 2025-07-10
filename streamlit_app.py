a6499e335b7fb0856fcfed2d28c6b79f18311007
merirosvot <merirosvot@outlook.com>
2025-07-10 10:13:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ else:
            data_frame = df,
            page_content_column = "Вопрос"
         )
-    if qa_submitted
+    if qa_submitted:
        documents = loader.load()
     st.write(documents)
 
