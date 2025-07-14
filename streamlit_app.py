6a0824e8ac68b57f46dd5f7d406507e37e662282
merirosvot <merirosvot@outlook.com>
2025-07-14 12:25:10 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("knowledge_page.py", title="First page", icon=":material/favorite:"),
-    st.Page("FAQ_page.py", title="Second page", icon="🔥"),
+    st.Page("knowledge_page.py", title="Тест базы знаний", icon=":material/favorite:"),
+    st.Page("FAQ_page.py", title="Тест FAQ", icon="🔥"),
 ])
 pg.run()
