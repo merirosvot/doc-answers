86e3410cdc84212d7b57c1301e474f0f43d7a9f0
merirosvot <merirosvot@outlook.com>
2025-07-14 11:32:29 +0300

    Update FAQ_app.py

--- FAQ_app.py
+++ FAQ_app.py
@@ -7,6 +7,14 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.document_loaders import DataFrameLoader
 from langchain_core.vectorstores import InMemoryVectorStore
+def page2():
+    st.title("Second page")
+
+pg = st.navigation([
+    st.Page("streamlit_app.py", title="First page", icon="🔥"),
+    st.Page(page2, title="Second page", icon=":material/favorite:"),
+])
+pg.run()
 
 # Show title and description.
 st.title("📄 Тестируем FAQ")
