7adcec9886adf9ace075f0fc8b14da3b6a49b759
merirosvot <merirosvot@outlook.com>
2025-07-14 13:57:20 +0300

    Update FAQ_page.py

--- FAQ_page.py
+++ FAQ_page.py
@@ -4,6 +4,7 @@ import langchain
 #from openai import OpenAI
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import OpenAI
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
